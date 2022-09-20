5973b210639d9fbee230c030a756d4d544ea7a2e
Jacob <jakeregister94@gmail.com>
2022-09-20 16:25:07 -0700

    modal nearly complete, envelope open/close js working, transitioning visibility incomplete

---
+++ app.js
@@ -0,0 +1,36 @@
+//template_ezrf6j9
+//service_5gg2gw9
+// hwFZxrP-CJNPfG8Hh
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+
+     emailjs
+         .sendForm(
+             'service_5gg2gw9',
+             'template_ezrf6j9',
+             event.target,
+             'hwFZxrP-CJNPfG8Hh'
+         ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible"
+         }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly at jacobregisterfese@gmail.com"
+            )
+         })
+}
+
+let isModalOpen = false;
+function toggleModal() {
+    if(isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
+}
